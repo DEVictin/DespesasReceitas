15357afeb6a845af3412f648836f1a72cd8c9dee
Victor <160972304+DEVictin@users.noreply.github.com>
2024-06-23 16:13:57 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.IO;
 using DespesasReceitas;
-using System.Text;
 
 class Program
 {
@@ -22,32 +21,32 @@ class Program
                     {
                         case "1":
                             Console.WriteLine("Do que se trata sua despesa importante? ");
-                            string gastoImportante = Console.ReadLine();
+                            string gasto = Console.ReadLine();
                             Console.WriteLine("Qual o valor da despesa: ");
-                            double valorDespesaImportante = double.Parse(Console.ReadLine());
+                            double valorDespesa = double.Parse(Console.ReadLine());
                             Console.WriteLine("Quando precisa pagar? ");
-                            DateTime dataDespesaImportante = DateTime.Parse(Console.ReadLine());
-                            Despesas despesaImportante = new DespesasImportantes(gastoImportante, valorDespesaImportante, dataDespesaImportante);
+                            DateTime dataDespesa = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+                            Despesas despesa = new DespesasImportantes(gasto, valorDespesa, dataDespesa);
 
-                            using (StreamWriter arquivo = File.AppendText("DespesasImportantes.txt"))
+                            using (StreamWriter arquivo = File.AppendText("C:\\Temp\\DespesasImportantes.txt"))
                             {
-                                string newLine = string.Format("{0};{1};{2}", despesaImportante.Gasto, despesaImportante.ValorDespesa, despesaImportante.DataDespesa);
+                                string newLine = string.Format("{0};{1};{2}", despesa.Gasto, despesa.ValorDespesa, despesa.DataDespesa);
                                 arquivo.WriteLine(newLine);
                             }
                             break;
 
                         case "2":
                             Console.WriteLine("Do que se trata sua despesa superflua? ");
-                            string gastoSuperfluo = Console.ReadLine();
+                            gasto = Console.ReadLine();
                             Console.WriteLine("Qual o valor da despesa: ");
-                            double valorDespesaSuperflua = double.Parse(Console.ReadLine());
+                            valorDespesa = double.Parse(Console.ReadLine());
                             Console.WriteLine("Quando precisa pagar? ");
-                            DateTime dataDespesaSuperflua = DateTime.Parse(Console.ReadLine());
-                            Despesas despesaSuperflua = new DespesasSuperfluas(gastoSuperfluo, valorDespesaSuperflua, dataDespesaSuperflua);
+                            dataDespesa = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+                            despesa = new DespesasSuperfluas(gasto, valorDespesa, dataDespesa);
 
-                            using (StreamWriter arquivo = File.AppendText("DespesasSuperfluas.txt"))
+                            using (StreamWriter arquivo = File.AppendText("C:\\Temp\\DespesasSuperfluas.txt"))
                             {
-                                string newLine = string.Format("{0};{1};{2}", despesaSuperflua.Gasto, despesaSuperflua.ValorDespesa, despesaSuperflua.DataDespesa);
+                                string newLine = string.Format("{0};{1};{2}", despesa.Gasto, despesa.ValorDespesa, despesa.DataDespesa);
                                 arquivo.WriteLine(newLine);
                             }
                             break;
@@ -61,10 +60,10 @@ class Program
                     Console.Write("Qual o valor da receita? ");
                     double valorReceita = double.Parse(Console.ReadLine());
                     Console.Write("Qual a data que você irá receber? ");
-                    DateTime dataReceita = DateTime.Parse(Console.ReadLine());
+                    DateTime dataReceita = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
                     Receitas newReceita = new Receitas(receita, valorReceita, dataReceita);
 
-                    using (StreamWriter arquivo = File.AppendText("Receitas.txt"))
+                    using (StreamWriter arquivo = File.AppendText("C:\\Temp\\Receitas.txt"))
                     {
                         string newLine = string.Format("{0};{1};{2}", newReceita.Receita, newReceita.ValorReceita, newReceita.DataReceita);
                         arquivo.WriteLine(newLine);
@@ -73,10 +72,74 @@ class Program
                     break;
 
                 case "3":
+                    Console.Clear();
+                    Console.WriteLine("====================================== Personal Finances ===");
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("Receitas: ");
+                    Console.WriteLine("============================================================");
+                    string[] linhas = File.ReadAllLines("C:\\Temp\\Receitas.txt");
+                    foreach (string linha in linhas)
+                    {
+                        string[] dados = linha.Split(';');
+                        Receitas receita1 = new Receitas(dados[0], double.Parse(dados[1]), DateTime.Parse(dados[2]));
+                        Console.WriteLine(receita1);
+                    }
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("Despesas Importantes: ");
+                    Console.WriteLine("============================================================");
+                    string[] linhas2 = File.ReadAllLines("C:\\Temp\\DespesasImportantes.txt");
+                    foreach (string linha in linhas2)
+                    {
+                        string[] dados = linha.Split(';');
+                        DespesasImportantes despesa1 = new DespesasImportantes(dados[0], double.Parse(dados[1]), DateTime.Parse(dados[2]));
+                        Console.WriteLine(despesa1);
+                    }
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("Despesas Superfluas: ");
+                    Console.WriteLine("============================================================");
+                    string[] linhas3 = File.ReadAllLines("C:\\Temp\\DespesasSuperfluas.txt");
+                    foreach (string linha in linhas3)
+                    {
+                        string[] dados = linha.Split(';');
+                        DespesasSuperfluas despesa1 = new DespesasSuperfluas(dados[0], double.Parse(dados[1]), DateTime.Parse(dados[2]));
+                        Console.WriteLine(despesa1);
+                    }
+                    Console.ReadKey(); 
+                    break;
+
+                case "4":
+                    File.Delete("C:\\Temp\\DespesasImportantes.txt");
+                    File.Create("C:\\Temp\\DespesasImportantes.txt");
+                    File.Delete("C:\\Temp\\DespesasSuperfluas.txt");
+                    File.Create("C:\\Temp\\DespesasSuperfluas.txt");
+                    Console.Clear();
+                    Console.WriteLine("====================================== Personal Finances ===");
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("================== Despesas Excluidas!======================");
+                    Console.WriteLine("============================================================");
                     Console.ReadKey();
                     break;
 
+                case "5":
+                    File.Delete("C:\\Temp\\Receitas.txt");
+                    File.Create("C:\\Temp\\Receitas.txt");
+                    Console.Clear();
+                    Console.WriteLine("====================================== Personal Finances ===");
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine("================== Receitas Excluidas!======================");
+                    Console.WriteLine("============================================================");
+                    Console.ReadKey();
+                    break;
+
+                case "6":
+                    return;
+
                 default:
+                    Console.WriteLine("Opção Inválida!");
+                    Console.ReadKey();
                     return;
             }
             n = "";
@@ -117,10 +180,6 @@ class Program
             Console.WriteLine("==================  Cadastrar Receitas  ====================");
             Console.WriteLine("============================================================");
 
-
         }
     }
 }
-
-    
-
