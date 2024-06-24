6a27cae837fd1a9a9471dc9edca618bf4f6aab69
Victor <160972304+DEVictin@users.noreply.github.com>
2024-06-24 17:05:55 -0300

    Update DespesasImportantes.cs

--- Entites/DespesasImportantes.cs
+++ Entites/DespesasImportantes.cs
@@ -9,7 +9,10 @@ namespace DespesasReceitas
         public DespesasImportantes(string gasto, double valorDespesa, DateTime dataDespesa) 
             : base(gasto, valorDespesa, dataDespesa)
         {
-
+        }
+        override public string ToString()
+        {
+            return string.Format("Gasto: {0}\nValor: R$ {1}; Data: {2}", Gasto, ValorDespesa, DataDespesa.ToString("dd/MM/yyyy"));
         }
     }
 }
